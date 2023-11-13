3ddb729b1cb991e22dab0a04c121d6fcdf0766ea
Sean LeCheminant <32964349+saltytostitos@users.noreply.github.com>
2023-11-12 23:41:06 -0600

    added icons

---
+++ components/icons/Bars2.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor"
+    height="1em"
+    width="1em"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M3.75 9h16.5m-16.5 6.75h16.5"
+    />
+  </svg>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
